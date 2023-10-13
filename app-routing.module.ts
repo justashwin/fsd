5f125431253876deedd0b8f07cc22f8df085124e
justashwin <146438851+justashwin@users.noreply.github.com>
2023-10-13 13:34:28 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppporudctComponent } from './appporudct/appporudct.component';
+
+const routes: Routes = [{path:'', component:HomeComponent},
+                        {path:'add', component:AppporudctComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
